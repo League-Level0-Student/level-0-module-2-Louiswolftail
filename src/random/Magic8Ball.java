a36cb527bbb5a8d1f2d7aa36d041e247742089f2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-05 13:53:16 -0800

    Magic8ball complete

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -13,27 +13,41 @@ public class Magic8Ball {
 	// 1. Make a main method that includes all the steps below….
 public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-	int r= new Random().nextInt(4);
+	int randomChoice =0;
+	Random Rover = new Random();
+	randomChoice=Rover.nextInt();
+	randomChoice=Rover.nextInt((2 - 0)+1)-1;
 	
 	// 3. Print out this variable
 
 	// 4. Get the user to enter a question for the 8 ball
 JOptionPane.showInputDialog("Ask a Question");
 	// 5. If the random number is 0
-
+if (randomChoice == 0) {
 	// -- tell the user "Yes"
-JOptionPane.showMessageDialog(null, "Yes! (☭ ͜ʖ ☭)");
+JOptionPane.showMessageDialog(null, "\\__  |   |\\_   _____//   _____/| |\n" + 
+		" /   |   | |    __)_ \\_____  \\ | |\n" + 
+		" \\____   | |        \\/        \\ \\|\n" + 
+		" / ______|/_______  /_______  / __\n" + 
+		" \\/               \\/        \\/  \\/");}
 
 	// 6. If the random number is 1
-
+else if (randomChoice == 1) {
 	// -- tell the user "No"
-JOptionPane.showConfirmDialog(null, "No! ( ‾ʖ̫‾)");
+JOptionPane.showMessageDialog(null, "░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░\n" + 
+		"░░░░█░░░░░░░░░░░░░█░░░░\n" + 
+		"░░░█░░░░░░░░░░▄▄▄░░█░░░\n" + 
+		"░░░█░░▄▄▄░░▄░░███░░█░░░\n" + 
+		"░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░\n" + 
+		"░░░█░░▀█▀█▀█▀█▀█▀░░█░░░\n" + 
+		"░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░\n" + 
+		"░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░ NO!"); }
 	// 7. If the random number is 2
-
+else if (randomChoice == 2) {
 	// -- tell the user "Maybe you should ask Google?"
-JOptionPane.showConfirmDialog(null, "Hey Google! Answer this question for me!");
+JOptionPane.showMessageDialog(null, "¯\\_(ツ)_/¯ Who Knows"); }
 	// 8. If the random number is 3
 
 	// -- write your own answer
 
-}
+}}
